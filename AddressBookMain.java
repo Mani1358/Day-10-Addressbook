0d65b257785658bd44901009bccf48d6a8e614e8
Mani1358 <mani621998@gmail.com>
2023-02-06 22:43:40 +0530

    Added:day-10 AddressBook

---
+++ AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.blz.day_10_assignment;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+        addressBook.editContact();
+        addressBook.deleteContact();
+        addressBook.checkDuplicateContact();
+        addressBook.searchByCityOrState();
+    }
+}
